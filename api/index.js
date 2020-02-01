d9015b7727706c16af3612911e956208c41d314d
James Byrne <mail@jamestbyrne.com>
2020-02-01 16:22:44 +0000

    Setup GraphQL server

--- api/index.js
+++ api/index.js
@@ -3,11 +3,14 @@ import session from "express-session";
 import uuid from "uuid/v4";
 import dotenv from "dotenv";
 import auth from "./auth";
+import { ApolloServer } from "apollo-server-express";
+import typeDefs from "./typeDefs";
+import resolvers from "./resolvers";
 
 dotenv.config();
 
 const PORT = 4000;
-const SESSION_SECRET = process.env.SESSION_SECRET;
+const SESSION_SECRET = process.env.REACT_APP_SESSION_SECRET;
 
 const app = express();
 
@@ -23,6 +26,17 @@ app.use(
 
 app.use(auth.initialize);
 app.use(auth.session);
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: ({ req }) => ({
+    getUser: () => req.user,
+    logout: () => req.logout()
+  })
+});
+
+server.applyMiddleware({ app });
 
 app.listen({ port: PORT }, () => {
   console.log(`Server ready at http://localhost:${PORT}`);
